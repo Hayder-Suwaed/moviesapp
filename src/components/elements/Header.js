9a8c8569eff59af056d4cfc471b3706756e13b81
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-03-31 00:24:56 -0600

    added header for home page

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React from "react";
+import { Link } from "@reach/router";
 
+import RMDBLogo from "../images/reactMovie_logo.png";
+import TMDBLogo from "../images/tmdb_logo.svg";
 
-const Header = () => <div>Header</div>
+import {
+  StyledHeader,
+  StyledRMDBLogo,
+  StyledTMDBLogo
+} from "../styles/StyledHeader";
 
-export default  Header;
+const Header = () => (
+  <StyledHeader>
+    <div className="header-content">
+      <Link to="/">
+        <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
+      <StyledTMDBLogo src={TMDBLogo} alt="tmdb-logo" />
+    </div>
+  </StyledHeader>
+);
+
+export default Header;
