f0d8412a2e19a59222b1f53465809e550a165cbc
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-04-01 12:38:42 -0600

    navigation and movieinfo

--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,5 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Link } from '@reach/router';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => <div>SearchBar</div>;
+import { StyledNavigation } from '../styles/StyledNavigation';
 
-export default SearchBar;
+const Navigation = ({ movie }) => (
+  <StyledNavigation>
+    <div className="navigation-content">
+      <Link to="/">
+        <p>Home</p>
+      </Link>
+      <p>|</p>
+      <p>{movie}</p>
+    </div>
+  </StyledNavigation>
+);
+
+Navigation.propTypes = {
+  movie: PropTypes.string,
+}
+
+export default Navigation;
