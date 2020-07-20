90b2991639c4629b7ecee448dcdae5083874d5c0
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-07-20 15:25:00 -0600

    cleanup code

--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { Link } from '@reach/router';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "@reach/router";
+import PropTypes from "prop-types";
 
-import { StyledMovieThump  } from '../styles/StyledMovieThumb';
+import { StyledMovieThump } from "../styles/StyledMovieThumb";
 
-const MovieThump  = ({ image, movieId, clickable }) => (
-  <StyledMovieThump >
+const MovieThump = ({ image, movieId, clickable }) => (
+  <StyledMovieThump>
     {clickable ? (
       <Link to={`/${movieId}`}>
         <img className="clickable" src={image} alt="moviethump" />
@@ -13,13 +13,13 @@ const MovieThump  = ({ image, movieId, clickable }) => (
     ) : (
       <img src={image} alt="moviethump" />
     )}
-  </StyledMovieThump >
-)
+  </StyledMovieThump>
+);
 
 MovieThump.propTypes = {
   image: PropTypes.string,
   movieId: PropTypes.number,
   clickable: PropTypes.bool,
-}
+};
 
 export default MovieThump;
