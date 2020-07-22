bfb8ed35c611cf04f11465d7a1a7a163d9e8d6c3
Hayder Suwaed <55573357+Hayder-Suwaed@users.noreply.github.com>
2020-07-21 23:32:06 -0600

    cleanup code

--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "@reach/router";
 import PropTypes from "prop-types";
-
 import { StyledNavigation } from "../styles/StyledNavigation";
 
 const Navigation = ({ movie }) => (
